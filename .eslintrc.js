1232149a15f253d680b2a10491868efc0d9a7f51
丘惟夫 <waveciou@gmail.com>
2022-07-17 17:51:16 +0800

    [dev] preserve podcast data

--- .eslintrc.js
+++ .eslintrc.js
@@ -12,11 +12,12 @@ module.exports = {
   plugins: [],
   // add your custom rules here
   rules: {
+    'vue/multi-word-component-names': ['off'],
     indent: ['off'],
     'linebreak-style': ['off', 'unix'],
     quotes: ['error', 'single'],
     semi: ['error', 'always'],
     'no-unused-vars': ['off'],
     'no-undef': ['off'],
-  }
+  },
 };
---
+++ layouts/default.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <p>666</p>
+    <Nuxt />
+  </div>
+</template>
+
+<script lang="ts">
+  import Vue from 'vue';
+
+  const X2JS = require('x2js');
+
+  export default Vue.extend({
+    mounted() {
+      (async () => {
+        try {
+          const { data } = await this.$axios({
+            method: 'get',
+            url: 'https://feeds.soundon.fm/podcasts/954689a5-3096-43a4-a80b-7810b219cef3.xml',
+            headers: { 'Content-Type': 'text/xml' },
+          });
+
+          const x2js = new X2JS();
+          const fetchData = x2js.xml2js(data);
+          console.log(fetchData);
+        } catch (error) {
+          console.error(error);
+        }
+      })();
+    },
+  });
+</script>
