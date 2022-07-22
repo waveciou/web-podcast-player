95039e297ba4ba153cc09fa18f16a6c7067e1be6
丘惟夫 <waveciou@gmail.com>
2022-07-22 21:43:13 +0800

    [dev] set fetch data

--- layouts/default.vue
+++ layouts/default.vue
@@ -6,8 +6,17 @@
 
 <script lang="ts">
   import Vue from 'vue';
+  import { IDetail, IEpisode } from '../store';
 
   const X2JS = require('x2js');
+
+  interface IOriginalData {
+    title: string;
+    description: string;
+    pubDate: string;
+    enclosure: { _url: string };
+    image: { _href: string };
+  }
 
   export default Vue.extend({
     mounted() {
@@ -21,8 +30,38 @@
 
           const x2js = new X2JS();
           const fetchData = x2js.xml2js(data);
+          const { author, title, description, image, item } =
+            fetchData.rss.channel;
 
-          this.$store.commit('SET_CHANNEL_LIST', fetchData.rss.channel.item);
+          const detail: IDetail = {
+            author: author.__cdata,
+            title,
+            description,
+            image: {
+              url: image[0].url,
+              title: image[0].title,
+            },
+          };
+
+          const episode: IEpisode[] = item.map(
+            (episodeItem: IOriginalData, index: number) => {
+              const { title, description, pubDate, enclosure, image } =
+                episodeItem;
+              const id = index;
+
+              return {
+                id,
+                title,
+                description,
+                enclosure: enclosure._url,
+                pubDate,
+                imageUrl: image._href,
+              };
+            }
+          );
+
+          this.$store.commit('SET_DETAIL_DATA', detail);
+          this.$store.commit('SET_EPISODE_DATA', episode);
         } catch (error) {
           // eslint-disable-next-line no-console
           console.error(error);
--- store/index.ts
+++ store/index.ts
@@ -1,14 +1,49 @@
 import { MutationTree } from 'vuex';
 
-export const state = () => ({
-  channelList: [],
+export interface IDetail {
+  author: string;
+  title: string;
+  description: string;
+  image: {
+    url: string;
+    title: string;
+  };
+}
+
+export interface IEpisode {
+  id: number;
+  title: string;
+  description: string;
+  enclosure: string;
+  pubDate: string;
+  imageUrl: string;
+}
+
+export const state = (): {
+  detail: IDetail;
+  episode: IEpisode[];
+} => ({
+  detail: {
+    author: '',
+    title: '',
+    description: '',
+    image: {
+      url: '',
+      title: '',
+    },
+  },
+  episode: [],
 });
 
 export type RootState = ReturnType<typeof state>;
 
 export const mutations: MutationTree<RootState> = {
-  SET_CHANNEL_LIST(state: RootState, payload: any) {
+  SET_DETAIL_DATA(state: RootState, payload: IDetail) {
     const assignState = state;
-    assignState.channelList = payload;
+    assignState.detail = JSON.parse(JSON.stringify(payload));
+  },
+  SET_EPISODE_DATA(state: RootState, payload: IEpisode[]) {
+    const assignState = state;
+    assignState.episode = [...payload];
   },
 };
