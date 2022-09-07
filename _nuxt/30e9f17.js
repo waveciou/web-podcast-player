9d1e7883d8ba96c45b62d05b55268a35c1435cf3
丘惟夫 <waveciou@gmail.com>
2022-09-07 20:33:51 +0800

    generate

--- _nuxt/30e9f17.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[4],{294:function(n,e,t){"use strict";t.r(e);var r={asyncData:function(n){n.route,n.query;return(0,n.redirect)("/")}},c=t(18),component=Object(c.a)(r,undefined,undefined,!1,null,null,null);e.default=component.exports}}]);
